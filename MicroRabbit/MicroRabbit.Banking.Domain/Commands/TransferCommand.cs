1cda5720d263b98b35cfe747658d21d366a378ae
Raghvendra Singh <th.raghav980@gmail.com>
2020-09-14 20:09:39 +0530

    Implemeted publish and send command for Banking Microsercies

---
+++ MicroRabbit/MicroRabbit.Banking.Domain/Commands/TransferCommand.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Commands
+{
+    public class TransferCommand : Command
+    {
+        public int From { get; protected set; }
+        public int TO { get; protected set; }
+        public decimal Amount { get; protected set; }
+    }
+}
